3b49a4bc4281bd2cef1caba57eaac397442698c6
00015541 <wiut00015541@gmail.com>
2024-12-05 10:42:05 +0500

    Categories Component created

--- ExpenseManagerAngular/src/app/categories/categories.component.ts
+++ ExpenseManagerAngular/src/app/categories/categories.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-categories',
-  standalone: false,
-  
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.css'
+  styleUrls: ['./categories.component.css']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  categories: any[] = [];
+  newCategory = { name: '' };
 
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.loadCategories();
+  }
+
+  loadCategories(): void {
+    this.apiService.getCategories().subscribe(data => {
+      this.categories = data;
+    });
+  }
+
+  addCategory(): void {
+    this.apiService.addCategory(this.newCategory).subscribe(() => {
+      this.loadCategories();
+      this.newCategory.name = '';
+    });
+  }
+
+  deleteCategory(id: number): void {
+    this.apiService.deleteCategory(id).subscribe(() => {
+      this.loadCategories();
+    });
+  }
 }
