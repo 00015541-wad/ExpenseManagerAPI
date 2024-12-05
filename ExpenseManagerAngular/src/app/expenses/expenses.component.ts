b7da1b7a8bbdd48cb3a5cd6c9fda3520c4bd4d21
00015541 <wiut00015541@gmail.com>
2024-12-05 10:43:18 +0500

    Expenses Component Created

--- ExpenseManagerAngular/src/app/expenses/expenses.component.ts
+++ ExpenseManagerAngular/src/app/expenses/expenses.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-expenses',
-  standalone: false,
-  
   templateUrl: './expenses.component.html',
-  styleUrl: './expenses.component.css'
+  styleUrls: ['./expenses.component.css']
 })
-export class ExpensesComponent {
+export class ExpensesComponent implements OnInit {
+  expenses: any[] = [];
+  newExpense = { title: '', amount: 0, categoryId: 0 };
 
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.loadExpenses();
+  }
+
+  loadExpenses(): void {
+    this.apiService.getExpenses().subscribe(data => {
+      this.expenses = data;
+    });
+  }
+
+  addExpense(): void {
+    this.apiService.addExpense(this.newExpense).subscribe(() => {
+      this.loadExpenses();
+      this.newExpense = { title: '', amount: 0, categoryId: 0 };
+    });
+  }
+
+  deleteExpense(id: number): void {
+    this.apiService.deleteExpense(id).subscribe(() => {
+      this.loadExpenses();
+    });
+  }
 }
